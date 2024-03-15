da73c3a6f163759b243a2ed588e8b268b01a6fa7
vedant1711 <vscode1711@gmai.com>
2024-03-15 23:53:01 +0530

    Revert "mistake commits
    This reverts commit 4cd57941fd87b4f596c93528bfe12159abacb667.

--- hackathon/models.py
+++ hackathon/models.py
@@ -3,23 +3,16 @@
 from django.db import models
 import uuid
 from django.utils import timezone
-from django.contrib.auth.models import AbstractUser
 
-
-class Team(AbstractUser):
-    username = None
-    team_name = models.CharField(unique=True, max_length=16)
-    # Add other relevant fields
+class Team(models.Model):
+    team_name = models.CharField(max_length=255)
     team_members = models.TextField()
     access_key = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+
+
+class ApiHitCount(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
     api_code = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     time_refreshed = models.DateTimeField(default=timezone.now)
     api_hits = models.PositiveIntegerField(default=0)
     timestamp = models.DateTimeField(auto_now_add=True)
-
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=True)
-    
-    REQUIRED_FIELDS = []
-
--- hackathon/serializers.py
+++ hackathon/serializers.py
@@ -1,7 +1,14 @@
+# hackathon/serializers.py
+
 from rest_framework import serializers
-from .models import Team
+from .models import Team, ApiHitCount
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
-        fields = ['id', 'username', 'name', 'team_members', 'api_code', 'time_refreshed']
+        fields = ['id', 'team_name', 'team_members', 'access_key']
+
+class ApiHitCountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApiHitCount
+        fields = ['api_code']
